a7e0bcc11e3aa6dc5d98563261147ec1ac0b8bf5
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-12-04 09:36:54 -0700

    Add authorication to TurboAdjudicator.so

--- contracts/TurboAdjudicator.sol
+++ contracts/TurboAdjudicator.sol
@@ -13,7 +13,31 @@ contract TurboAdjudicator {
         uint challengeDuration;
     }
 
+    struct Authorization {
+        // *********************************************************
+        // WARNING
+        // -------
+        // This authorization does not prevent collisions.
+        // It only prevents griefing: if the participant signs
+        // this authorization, we assume they wish to withdraw
+        // the amount to the destination. Therefore, any front-
+        // runner who submits the transaction in lieu of the
+        // participant is simply saving them some gas.
+        // (If the amount were not part of the authorization,
+        // then a malicious actor could submit a withdrawal
+        // for a tiny amount, forcing alice to submit a new
+        // signature with an increased nonce. If they have to
+        // submit the amount that the participant authorized,
+        // then the participant can be satisfied with the front-
+        // runner's transaction, which causes the desired transfer.)
+        // *********************************************************
+        address destination;
+        uint amount;
+        uint nonce;
+    }
+
     mapping(address => uint) public allocations;
+    mapping(address => uint) public withdrawalNonce;
 
     function deposit(address destination, uint amount) public payable {
         require(
@@ -22,5 +46,31 @@ contract TurboAdjudicator {
         );
 
         allocations[destination] = allocations[destination] + amount;
+    }
+
+    function withdraw(address participant, address payable destination, uint amount, bytes32 signedMessage, uint8 _v, bytes32 _r, bytes32 _s) public payable {
+        require(
+            allocations[participant] >= amount,
+            "Withdraw: overdrawn"
+        );
+        require(
+            ecrecover(signedMessage, _v, _r, _s) == participant,
+            "Withdraw: not authorized by fromParticipant"
+        );
+
+
+        Authorization memory authorization = Authorization(
+            destination,
+            amount,
+            withdrawalNonce[participant]
+        );
+        require(
+            signedMessage == keccak256(abi.encode(authorization)),
+            "Withdraw: invalid authorization"
+        );
+
+        withdrawalNonce[participant] = withdrawalNonce[participant] + 1;
+        allocations[participant] = allocations[participant] - amount;
+        destination.transfer(amount);
     }
 }
