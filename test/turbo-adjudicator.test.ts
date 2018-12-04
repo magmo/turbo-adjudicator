61e8c7718b1144d4993a8731f48e2ce098932bfa
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-12-04 09:38:02 -0700

    WIP: tests

--- test/turbo-adjudicator.test.ts
+++ test/turbo-adjudicator.test.ts
@@ -4,17 +4,22 @@ import SimpleAdjudicatorArtifact from '../build/contracts/TurboAdjudicator.json'
 import { ContractFactory } from 'ethers';
 import { linkedByteCode, expectRevert, walletWithEthAndProvider as wallet, getNetworkId, ganacheProvider as provider  } from 'magmo-devtools';
 
-import { channel, } from "./test-scenarios";
+import { channel, alice, bob, aliceDest } from "./test-scenarios";
+import { sign } from "fmg-core";
+
+import { soliditySha3 } from "web3-utils";
+
 
 jest.setTimeout(20000);
 let turbo;
 
-const DEPOSIT_AMOUNT = 20;
+const DEPOSIT_AMOUNT = 255; // 
 const SMALL_WITHDRAW_AMOUNT = 10;
-const LARGE_WITHDRAW_AMOUNT = 30;
+
+const MESSAGE = soliditySha3("foo");
 
 function depositTo(destination, amount=DEPOSIT_AMOUNT, value=DEPOSIT_AMOUNT): Promise<any> {
-  return turbo.deposit(destination, DEPOSIT_AMOUNT, { value: DEPOSIT_AMOUNT });
+  return turbo.deposit(destination, amount, { value: DEPOSIT_AMOUNT });
 }
 
 describe('SimpleAdjudicator', () => {
@@ -32,38 +37,50 @@ describe('SimpleAdjudicator', () => {
       await depositTo(channel.id);
       const allocatedAmount  = await turbo.allocations(channel.id);
 
-      expect(allocatedAmount.toNumber()).toEqual(20);
+      expect(allocatedAmount.toNumber()).toEqual(DEPOSIT_AMOUNT);
     });
 
     it("requires deposit amount to match msg.value", async () => {
-      expectRevert(depositTo(channel.id, DEPOSIT_AMOUNT, DEPOSIT_AMOUNT + 1), "deposit amount must match msg.value");
+      // should fail
+      await depositTo(channel.id, DEPOSIT_AMOUNT, DEPOSIT_AMOUNT + 1);
     });
   });
 
   describe("withdraw", () => {
-    it("works when allocations[fromParticipant] >= amount and sent on behalf of fromParticipant", async () => {
+    it.skip("works when allocations[fromParticipant] >= amount and sent on behalf of fromParticipant", async () => {
       await depositTo(alice.address);
 
       const startBal = await provider.getBalance(aliceDest.address);
       const allocatedAtStart = await turbo.allocations(alice.address); // should be at least DEPOSIT_AMOUNT, regardless of test ordering
 
-      await turbo.withdraw(alice.address, aliceDest.address, SMALL_WITHDRAW_AMOUNT, alice.signature);
+      // Alice can withdraw some of her money
+      await withdraw(alice, aliceDest.address, alice, SMALL_WITHDRAW_AMOUNT);
 
-      expect(await provider.getBalance(aliceDest.address)).toEqual(startBal + SMALL_WITHDRAW_AMOUNT);
-      expect(await turbo.allocations(alice.address)).toEqual(allocatedAtStart - SMALL_WITHDRAW_AMOUNT);
+      expect(Number(await provider.getBalance(aliceDest.address))).toEqual(Number(startBal.add(SMALL_WITHDRAW_AMOUNT)));
+      expect(Number(await turbo.allocations(alice.address))).toEqual(Number(allocatedAtStart - SMALL_WITHDRAW_AMOUNT));
 
       // Alice should be able to withdraw all remaining funds allocated to her.
-      await turbo.withdraw(alice.address, aliceDest.address, allocatedAtStart - SMALL_WITHDRAW_AMOUNT, alice.signature);
+      await withdraw(alice, aliceDest.address, alice, allocatedAtStart - SMALL_WITHDRAW_AMOUNT);
+
+      expect(Number(await provider.getBalance(aliceDest.address))).toEqual(Number(await provider.getBalance(aliceDest.address)));
+      expect(Number(await turbo.allocations(alice.address))).toEqual(0);
     });
 
     it("reverts when allocations[fromParticipant] > amount but not sent on behalf of fromParticipant", async () => {
       await depositTo(alice.address);
-      expectRevert(turbo.withdraw(alice.address, aliceDest.address, SMALL_WITHDRAW_AMOUNT, bob.signature), "Withdraw: not authorized by fromParticipant");
+      expectRevert(withdraw(alice, aliceDest.address, bob), "Withdraw: not authorized by fromParticipant");
     });
 
     it("reverts when sent on behalf of fromParticipant but allocations[fromParticipant] < amount", async () => {
       await depositTo(alice.address);
-      expectRevert(turbo.withdraw(alice.address, aliceDest.address, LARGE_WITHDRAW_AMOUNT, alice.signature), "Withdraw: overdrawn");
+      const allocated = await turbo.allocations(alice.address); // should be at least DEPOSIT_AMOUNT, regardless of test ordering
+      expectRevert(withdraw(alice, aliceDest.address, alice, Number(allocated) + 100000));
     });
   });
 });
+
+async function withdraw(participant, destination, signer=participant, amount=DEPOSIT_AMOUNT): Promise<any> {
+  const sig: any = sign(MESSAGE, signer.privateKey);
+  return turbo.withdraw(participant.address, destination, amount, sig.messageHash, sig.v, sig.r, sig.s, { gasLimit: 3000000 });
+
+}
