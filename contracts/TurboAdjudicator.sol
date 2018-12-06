c0e762c7f92fd0bd8f5ccb681c0c154639f20a53
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-12-06 08:07:55 -0700

    Implement transfer

--- contracts/TurboAdjudicator.sol
+++ contracts/TurboAdjudicator.sol
@@ -25,8 +25,15 @@ contract TurboAdjudicator {
         uint nonce;
     }
 
+    struct Outcome {
+        address[] destination;
+        uint[] amount;
+        bool isFinal;
+    }
+
     mapping(address => uint) public allocations;
     mapping(address => uint) public withdrawalNonce;
+    mapping(address => Outcome) public outcomes;
 
     function deposit(address destination) public payable {
         allocations[destination] = allocations[destination] + msg.value;
@@ -57,9 +64,78 @@ contract TurboAdjudicator {
         destination.transfer(amount);
     }
 
-    function recover(bytes32 m, uint8 v, bytes32 r, bytes32 s) public pure returns (address) {
-        return ecrecover(m, v, r, s);
+    function transfer(address channel, address destination, uint amount) public {
+        require(
+            outcomes[channel].isFinal,
+            "Transfer: outcome must be final"
+        );
+
+        uint256 pending = 0;
+        for (uint256 i = 0; i < outcomes[channel].destination.length; i++) {
+            pending = pending + outcomes[channel].amount[i];
+
+            if (outcomes[channel].destination[i] == destination) {
+                require(
+                    pending <= allocations[channel],
+                    "Transfer: allocations[channel] must cover transfer"
+                );
+
+                require(
+                    amount <= outcomes[channel].amount[i],
+                    "Transfer: transfer too large"
+                );
+
+                allocations[destination] = allocations[destination] + amount;
+                allocations[channel] = allocations[channel] - amount;
+
+                uint256[] memory updatedAmounts = outcomes[channel].amount;
+                updatedAmounts[i] = updatedAmounts[i] - amount;
+
+                Outcome memory updatedOutcome = Outcome(
+                    outcomes[channel].destination,
+                    updatedAmounts,
+                    outcomes[channel].isFinal
+                );
+                outcomes[channel] = updatedOutcome;
+                return;
+            }
+        }
+
+        revert("Transfer: destination not in outcome");
     }
+
+    // Helper functions
+    function setOutcome(address channel, Outcome memory outcome) public {
+        // Temporary helper function to set outcomes for testing
+        // Will eventually be internal
+        require(
+            channel != address(this),
+            "Invalid channel"
+        );
+
+        if (!outcomesEqual(outcomes[channel], outcomes[address(this)])) {
+            require(
+                equals(abi.encode(outcome.destination), abi.encode(outcomes[channel].destination)),
+                "destination must match existing outcome"
+            );
+        }
+
+        require(
+            outcome.destination.length == outcome.amount.length,
+            "destination.length must be equal to amount.length"
+        );
+
+        outcomes[channel] = outcome;
+    }
+
+    function outcomesEqual(Outcome memory a, Outcome memory b) internal pure returns (bool) {
+        return equals(abi.encode(a), abi.encode(b));
+    }
+
+    function equals(bytes memory a, bytes memory b) internal pure returns (bool) {
+        return keccak256(a) == keccak256(b);
+    }
+
 
     function recoverSigner(bytes memory _d, uint8 _v, bytes32 _r, bytes32 _s) internal pure returns(address) {
         bytes memory prefix = "\x19Ethereum Signed Message:\n32";
