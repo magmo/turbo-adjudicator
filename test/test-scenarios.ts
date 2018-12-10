a4131e43f753e633bf5630e8143d3da8589a1620
Andrew Stewart <andrew.gord.stewart@gmail.com>
2018-12-09 17:02:45 -0700

    Update test-scenarios

--- test/test-scenarios.ts
+++ test/test-scenarios.ts
@@ -1,5 +1,7 @@
 import { ethers } from "ethers";
-import { Channel } from "fmg-core";
+import { Channel, State, CountingGame, sign } from "fmg-core";
+
+import BN from "bn.js";
 
 const aBal = ethers.utils.parseUnits('6', 'wei');
 const bBal = ethers.utils.parseUnits('4', 'wei');
@@ -15,4 +17,65 @@ export const channel = new Channel(
     ethers.Wallet.createRandom().address,
     0,
     [alice.address, bob.address]
-);
+);const defaults = { channel, resolution, gameCounter: 0 };
+
+export const state0 = CountingGame.gameState({
+    ...defaults,
+    gameCounter: 1,
+    turnNum: 6,
+});
+export const state1 = CountingGame.gameState({
+    channel,
+    resolution,
+    turnNum: 7,
+    gameCounter: 2,
+});
+export const state2 = CountingGame.gameState({
+    channel,
+    resolution,
+    turnNum: 8,
+    gameCounter: 3,
+});
+export const state3 = CountingGame.gameState({
+    channel,
+    resolution,
+    turnNum: 9,
+    gameCounter: 4,
+});
+
+export const state1alt = CountingGame.gameState({
+    channel,
+    resolution: differentResolution,
+    turnNum: 7,
+    gameCounter: 2,
+});
+export const state2alt = CountingGame.gameState({
+    channel,
+    resolution: differentResolution,
+    turnNum: 8,
+    gameCounter: 3,
+});
+
+export function conclusionProof(conclusionResolution?: BN[]) {
+    //
+    const _channel = randomChannel();
+    const aliceState = state0;
+    const bobState = state1;
+    aliceState.stateType = State.StateType.Conclude;
+    bobState.stateType = State.StateType.Conclude;
+    aliceState.channel = _channel;
+    bobState.channel = _channel;
+    if (conclusionResolution) {
+        aliceState.resolution = conclusionResolution;
+        bobState.resolution = conclusionResolution;
+    }
+    const { r: r0, s: s0, v: v0 } = sign(aliceState.toHex(), alice.privateKey);
+    const { r: r1, s: s1, v: v1 } = sign(bobState.toHex(), bob.privateKey); 
+
+    return {
+        penultimateState: aliceState.asEthersObject,
+        ultimateState: bobState.asEthersObject,
+        penultimateSignature: { v: v0, r: r0, s: s0 },
+        ultimateSignature: { v: v1, r: r1, s: s1 },
+    };
+}
